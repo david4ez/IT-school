c7be2c2781fb5886b9f7deee3d00ee1e1a32dc3a
david4ez <54486484+david4ez@users.noreply.github.com>
2021-07-20 15:55:03 +0300

    Update task_1.cpp

--- task_1/task_1.cpp
+++ task_1/task_1.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
+#include <memory>
+#include <sstream>
 #include <openssl/pem.h>
 #include <openssl/x509.h>
+#include <exception>
 
 using namespace std;
 
-struct Data {
-    BIO* bio;
-    X509* x;
+class Data {
+public:
+    Data() = delete;
+    Data(const char* name) 
+    : fileName(name) {
+        try {
+            if (BIO_new_file(fileName, "rb") == NULL)
+                throw runtime_error("Unable to open the certificate");
+        }
+        catch (const runtime_error& e) {
+            cout << e.what();
+
+            exit(0);
+        }
+    }
+
+    string sha256() const {
+        static const char hexbytes[] = "0123456789ABCDEF";
+        unsigned int n;
+        unsigned char md[EVP_MAX_MD_SIZE];
+        stringstream ss;
+        string result;
+
+        X509_digest(x.get(), EVP_sha256(), md, &n);
+
+        for (int i = 0; i < n; i++) {
+            ss << hexbytes[(md[i] & 0xf0) >> 4] << hexbytes[(md[i] & 0x0f) >> 0];
+            if (i != n - 1)
+                ss << ":";
+        }
+
+        ss >> result;
+
+        return result;
+    }
+private:
+    const char* fileName;
+
+    unique_ptr<BIO, decltype(&BIO_free)> bio{ BIO_new_file(fileName, "rb"), &BIO_free };
+    unique_ptr<X509, decltype(&X509_free)> x{ PEM_read_bio_X509(bio.get(), NULL, NULL, NULL), &X509_free };
 };
 
 int main() {
-    Data info;
+    char fileName[80];
+    cin >> fileName;
 
-    info.bio = BIO_new_file("certificate.pem", "rb");
-    info.x = PEM_read_bio_X509(info.bio, NULL, NULL, NULL);
+    Data info(fileName);
 
-    static const char hexbytes[] = "0123456789ABCDEF";
-    unsigned int n;
-    unsigned char md[EVP_MAX_MD_SIZE];
-
-    X509_digest(info.x, EVP_sha256(), md, &n);
-
-    cout << "SHA256 Fingerprint=";
-
-    for (int i = 0; i < n; i++) {
-        cout << hexbytes[(md[i] & 0xf0) >> 4] << hexbytes[(md[i] & 0x0f) >> 0];
-        if (i != n - 1)
-            cout << ":";
-    }
-
-    cout << endl;
+    cout << "SHA256 Fingerprint=" << info.sha256() << endl;
 
     return EXIT_SUCCESS;
 }
